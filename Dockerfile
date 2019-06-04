ac7d88cf5eeeb392f19a4bdd01bfc9ff6f35d574
Andrey Beletsky <andrey@vr2.net>
2019-06-05 01:56:02 +0700

    Optimize lbrynet configuration at launch

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
 FROM ubuntu:18.04
-EXPOSE 5279
+EXPOSE 5279 5280
+
 VOLUME /storage
-RUN apt-get update && apt-get -y install ca-certificates
+RUN apt-get update && apt-get -y install ca-certificates bash
 RUN mkdir /daemon
 WORKDIR /daemon
 COPY lbrynet ./
 COPY conf/daemon_settings.yml ./
 COPY conf/test_daemon_settings.yml ./
-ENV LBRY_DATA_DIR /storage/data
-ENV LBRY_LBRYUM_WALLET_DIR /storage/lbryum
-ENV LBRY_DOWNLOAD_DIRECTORY /storage/download
-ENV LBRY_API 0.0.0.0:5279
+
 COPY start.sh ./start.sh
 RUN chmod a+x start.sh
 CMD ["./start.sh"]
--- start.sh
+++ start.sh
@@ -1,19 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
 set -o xtrace
+set -e
 
-if [ ! -d "$LBRY_DATA_DIR" ]; then
-    mkdir $LBRY_DATA_DIR
-fi
-if [ ! -d "$LBRY_LBRYUM_WALLET_DIR" ]; then
-    mkdir $LBRY_LBRYUM_WALLET_DIR
-fi
-if [ ! -d "$LBRY_DOWNLOAD_DIRECTORY" ]; then
-    mkdir $LBRY_DOWNLOAD_DIRECTORY
-fi
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
 
 if [ -z ${LBRY_DOCKER_CONFIG+x} ]; then
-    ./lbrynet start --config=/daemon/daemon_settings.yml
+    CONFIG="/daemon/daemon_settings.yml"
+    
 else
-    ./lbrynet start --config=$LBRY_DOCKER_CONFIG
+    CONFIG=$LBRY_DOCKER_CONFIG
 fi
+
+cat $CONFIG
+
+eval $(parse_yaml ${CONFIG} "LBRY_")
+
+if [ ! -d "$LBRY_data_dir" ]; then
+    mkdir $LBRY_data_dir
+fi
+if [ ! -d "$LBRY_wallet_dir" ]; then
+    mkdir $LBRY_wallet_dir
+fi
+if [ ! -d "$LBRY_download_dir" ]; then
+    mkdir $LBRY_download_dir
+fi
+
+./lbrynet start --config=$CONFIG
